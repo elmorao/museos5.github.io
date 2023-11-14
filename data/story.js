e203f21732d5f429eb1e925fc30d1faeca9b4e0a
nataliaU26 <nataliacamilaumana@gmail.com>
2023-11-14 09:03:01 -0500

    info

---
+++ data/story.js
@@ -0,0 +1,240 @@
+var story = {
+ "docName": "wireframes",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "hideGallery": false,
+ "galleryPageColorsEnabled": true,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "title": "wireframes",
+ "layersExist": true,
+ "pages": [
+  {
+   "id": "104:331",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "0",
+   "image": "0.png",
+   "index": 0,
+   "width": 1567,
+   "height": 1478,
+   "x": -1585,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Boton",
+     "rect": {
+      "x": 680.5,
+      "y": 998.5480346679688,
+      "width": 291,
+      "height": 80
+     },
+     "index": 0,
+     "page": 1,
+     "disableAutoScroll": true
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:1088",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "0.1",
+   "image": "0.1.png",
+   "index": 1,
+   "width": 1567,
+   "height": 1478,
+   "x": 22,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "luis p",
+     "rect": {
+      "x": 139.5338134765625,
+      "y": 953.5894775390625,
+      "width": 396.4276123046875,
+      "height": 428.20172119140625
+     },
+     "index": 1,
+     "page": 2
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:1715",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f0",
+   "image": "f0.png",
+   "index": 2,
+   "width": 1567,
+   "height": 1478,
+   "x": 1724,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Boton_siguiente",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 2,
+     "page": 3
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 3,
+     "page": 1
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 4,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:1295",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f1",
+   "image": "f1.png",
+   "index": 3,
+   "width": 1567,
+   "height": 1478,
+   "x": 3369,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 5,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:1835",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2",
+   "image": "f2.png",
+   "index": 4,
+   "width": 1567,
+   "height": 1478,
+   "x": 4976,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 6,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:1946",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(1)",
+   "image": "f2(1).png",
+   "index": 5,
+   "width": 1567,
+   "height": 1478,
+   "x": 6583,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 7,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  }
+ ],
+ "groups": [
+  {
+   "id": "0:1",
+   "name": "Page 1",
+   "backColor": "#1E1E1E"
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 6
+}
