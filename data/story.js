45e691748e3f0047e04ae43527e45bef0aa79ca1
nataliaU26 <nataliacamilaumana@gmail.com>
2023-11-14 10:16:31 -0500

    interactive_info

--- data/story.js
+++ data/story.js
@@ -66,6 +66,28 @@ var story = {
    "fixedPanels": [],
    "links": [
     {
+     "name": "luis m",
+     "rect": {
+      "x": 332.96453857421875,
+      "y": 309.3021240234375,
+      "width": 427.5797119140625,
+      "height": 396.81842041015625
+     },
+     "index": 1,
+     "page": 2
+    },
+    {
+     "name": "francisco m",
+     "rect": {
+      "x": 139.55859375,
+      "y": 502.68756103515625,
+      "width": 396.551513671875,
+      "height": 428.0528259277344
+     },
+     "index": 2,
+     "page": 3
+    },
+    {
      "name": "luis p",
      "rect": {
       "x": 139.5338134765625,
@@ -73,8 +95,114 @@ var story = {
       "width": 396.4276123046875,
       "height": 428.20172119140625
      },
-     "index": 1,
-     "page": 2
+     "index": 3,
+     "page": 4
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:3529",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f0",
+   "image": "f0.png",
+   "index": 2,
+   "width": 1567,
+   "height": 1478,
+   "x": 1722,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Boton_siguiente",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 4,
+     "page": 5
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 5,
+     "page": 1
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 6,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2861",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f0(1)",
+   "image": "f0(1).png",
+   "index": 3,
+   "width": 1567,
+   "height": 1478,
+   "x": 1723,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Boton_siguiente",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 7,
+     "page": 6
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 8,
+     "page": 1
+    },
+    {
+     "name": "Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 9,
+     "page": 1
     }
    ],
    "overlayPinPage": 6,
@@ -85,9 +213,9 @@ var story = {
    "id": "105:1715",
    "groupID": "0:1",
    "transAnimType": 0,
-   "title": "f0",
-   "image": "f0.png",
-   "index": 2,
+   "title": "f0(2)",
+   "image": "f0(2).png",
+   "index": 4,
    "width": 1567,
    "height": 1478,
    "x": 1724,
@@ -104,8 +232,8 @@ var story = {
       "width": 90,
       "height": 90
      },
-     "index": 2,
-     "page": 3
+     "index": 10,
+     "page": 7
     },
     {
      "name": "Boton_cerrar",
@@ -115,7 +243,7 @@ var story = {
       "width": 100,
       "height": 100
      },
-     "index": 3,
+     "index": 11,
      "page": 1
     },
     {
@@ -126,7 +254,113 @@ var story = {
       "width": 100,
       "height": 100
      },
-     "index": 4,
+     "index": 12,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2977",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f1",
+   "image": "f1.png",
+   "index": 5,
+   "width": 1567,
+   "height": 1478,
+   "x": 3367,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 38",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 13,
+     "page": 2
+    },
+    {
+     "name": "Frame 37",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 14,
+     "page": 8
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 15,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2309",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f1(1)",
+   "image": "f1(1).png",
+   "index": 6,
+   "width": 1567,
+   "height": 1478,
+   "x": 3368,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 28",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 16,
+     "page": 3
+    },
+    {
+     "name": "Frame 27",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 17,
+     "page": 9
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 18,
      "page": 1
     }
    ],
@@ -138,9 +372,9 @@ var story = {
    "id": "105:1295",
    "groupID": "0:1",
    "transAnimType": 0,
-   "title": "f1",
-   "image": "f1.png",
-   "index": 3,
+   "title": "f1(2)",
+   "image": "f1(2).png",
+   "index": 7,
    "width": 1567,
    "height": 1478,
    "x": 3369,
@@ -150,6 +384,28 @@ var story = {
    "fixedPanels": [],
    "links": [
     {
+     "name": "Frame 18",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 19,
+     "page": 4
+    },
+    {
+     "name": "Frame 17",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 20,
+     "page": 10
+    },
+    {
      "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
      "rect": {
       "x": 1326,
@@ -157,7 +413,115 @@ var story = {
       "width": 100,
       "height": 100
      },
-     "index": 5,
+     "index": 21,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:3085",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2",
+   "image": "f2.png",
+   "index": 8,
+   "width": 1567,
+   "height": 1478,
+   "x": 4974,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 40",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 22,
+     "page": 5
+    },
+    {
+     "name": "Frame 39",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 23,
+     "page": 11,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 24,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2417",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(1)",
+   "image": "f2(1).png",
+   "index": 9,
+   "width": 1567,
+   "height": 1478,
+   "x": 4975,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 29",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 25,
+     "page": 6
+    },
+    {
+     "name": "Frame 30",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 26,
+     "page": 12,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 27,
      "page": 1
     }
    ],
@@ -169,9 +533,9 @@ var story = {
    "id": "105:1835",
    "groupID": "0:1",
    "transAnimType": 0,
-   "title": "f2",
-   "image": "f2.png",
-   "index": 4,
+   "title": "f2(2)",
+   "image": "f2(2).png",
+   "index": 10,
    "width": 1567,
    "height": 1478,
    "x": 4976,
@@ -181,6 +545,29 @@ var story = {
    "fixedPanels": [],
    "links": [
     {
+     "name": "Frame 20",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 28,
+     "page": 7
+    },
+    {
+     "name": "Frame 19",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 29,
+     "page": 13,
+     "disableAutoScroll": true
+    },
+    {
      "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
      "rect": {
       "x": 1326,
@@ -188,7 +575,117 @@ var story = {
       "width": 100,
       "height": 100
      },
-     "index": 6,
+     "index": 30,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:3196",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(3)",
+   "image": "f2(3).png",
+   "index": 11,
+   "width": 1567,
+   "height": 1478,
+   "x": 6581,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 42",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 31,
+     "page": 8,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 41",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 32,
+     "page": 14,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 33,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2528",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(4)",
+   "image": "f2(4).png",
+   "index": 12,
+   "width": 1567,
+   "height": 1478,
+   "x": 6582,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 32",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 34,
+     "page": 9,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 31",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 35,
+     "page": 15,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 36,
      "page": 1
     }
    ],
@@ -200,9 +697,9 @@ var story = {
    "id": "105:1946",
    "groupID": "0:1",
    "transAnimType": 0,
-   "title": "f2(1)",
-   "image": "f2(1).png",
-   "index": 5,
+   "title": "f2(5)",
+   "image": "f2(5).png",
+   "index": 13,
    "width": 1567,
    "height": 1478,
    "x": 6583,
@@ -212,6 +709,30 @@ var story = {
    "fixedPanels": [],
    "links": [
     {
+     "name": "Frame 22",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 37,
+     "page": 10,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 21",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 38,
+     "page": 16,
+     "disableAutoScroll": true
+    },
+    {
      "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
      "rect": {
       "x": 1326,
@@ -219,7 +740,301 @@ var story = {
       "width": 100,
       "height": 100
      },
-     "index": 7,
+     "index": 39,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:3307",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(6)",
+   "image": "f2(6).png",
+   "index": 14,
+   "width": 1567,
+   "height": 1478,
+   "x": 8188,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 44",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 40,
+     "page": 11,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 43",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 41,
+     "page": 17,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 42,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2639",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(7)",
+   "image": "f2(7).png",
+   "index": 15,
+   "width": 1567,
+   "height": 1478,
+   "x": 8189,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 33",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 43,
+     "page": 12,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 34",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 44,
+     "page": 18,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 45,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2086",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(8)",
+   "image": "f2(8).png",
+   "index": 16,
+   "width": 1567,
+   "height": 1478,
+   "x": 8190,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 24",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 46,
+     "page": 13,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Frame 23",
+     "rect": {
+      "x": 1285,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 47,
+     "page": 19,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 48,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:3418",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(9)",
+   "image": "f2(9).png",
+   "index": 17,
+   "width": 1567,
+   "height": 1478,
+   "x": 9795,
+   "y": 3303,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 46",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 49,
+     "page": 14,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 50,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2750",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(10)",
+   "image": "f2(10).png",
+   "index": 18,
+   "width": 1567,
+   "height": 1478,
+   "x": 9796,
+   "y": 1587,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 36",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 51,
+     "page": 15,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 52,
+     "page": 1
+    }
+   ],
+   "overlayPinPage": 6,
+   "overlayCloseOnClickOutside": false,
+   "layout": null
+  },
+  {
+   "id": "105:2197",
+   "groupID": "0:1",
+   "transAnimType": 0,
+   "title": "f2(11)",
+   "image": "f2(11).png",
+   "index": 19,
+   "width": 1567,
+   "height": 1478,
+   "x": 9797,
+   "y": -129,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [
+    {
+     "name": "Frame 26",
+     "rect": {
+      "x": 219,
+      "y": 807,
+      "width": 90,
+      "height": 90
+     },
+     "index": 53,
+     "page": 16,
+     "disableAutoScroll": true
+    },
+    {
+     "name": "Slide 8_recuadro 2_Tema 1.2_Boton_cerrar",
+     "rect": {
+      "x": 1326,
+      "y": 422,
+      "width": 100,
+      "height": 100
+     },
+     "index": 54,
      "page": 1
     }
    ],
@@ -236,5 +1051,5 @@ var story = {
   }
  ],
  "startPageIndex": 0,
- "totalImages": 6
+ "totalImages": 20
 }
